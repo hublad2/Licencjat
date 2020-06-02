c93ea223093b5227a00fb85477c5e604c5f722fb
hublad2 <hublad@wp.pl>
2020-06-02 12:52:23 +0200

    Added firebase hosting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import { getHTMLZad1 } from "./Zadanie1";
 import { getHTMLZad2 } from "./Zadanie2";
 import { appendScripts, removeScripts } from "./scriptAppend";
 import { initListenersType1 } from "./InitType1";
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCD80AF1aUd88KAiC-FfHC0xEj_H2JEvXA",
+  authDomain: "zadania-b5048.firebaseapp.com",
+  databaseURL: "https://zadania-b5048.firebaseio.com",
+  projectId: "zadania-b5048",
+  storageBucket: "zadania-b5048.appspot.com",
+  messagingSenderId: "126515339750",
+  appId: "1:126515339750:web:24a16b746b4595a9823627",
+  measurementId: "G-0LVDYJC829",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const container = document.querySelector(".container");
 const spisZadan = document.querySelector("#spis-zadan");
